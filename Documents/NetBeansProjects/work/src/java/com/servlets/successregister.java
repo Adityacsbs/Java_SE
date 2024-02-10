8a99428733265d17ec23267913e79b0b8f986212
AADITYARAJ SINGH CHOUHAN <aadityarajsingh0903@gmail.com>
2024-02-10 14:22:50 +0530

    servlet practice1

--- Documents/NetBeansProjects/work/src/java/com/servlets/successregister.java
+++ Documents/NetBeansProjects/work/src/java/com/servlets/successregister.java
@@ -19,9 +19,21 @@ public class successregister extends HttpServlet {
         
         ok.println("<h1>You are successfully registered here</h1>");
         
-       ok.println("<a href=\"/RegisterServlet\"><h1>click here</h1></a>");
+       ok.println("<a href=\"/work/RegisterServlet\"><h1>click here</h1></a>");
 
     }
+    
+//      @Override
+//    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+// resp.setContentType("text/html");
+//        
+//        PrintWriter ok = resp.getWriter();
+//        
+//        ok.println("<h1>You are successfully registered here</h1>");
+//        
+//       ok.println("<a href=\"/work/RegisterServlet\"><h1>click here</h1></a>");
+//
+//    }
     
     
 }
